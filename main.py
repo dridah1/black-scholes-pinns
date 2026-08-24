4a4af536eb626d96be780b468eee9b551d0c3edd
Reda DLIMI <mr.reda.dlimi@gmail.com>
2026-08-25 00:06:08 +0100

    Create new project entry point

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Main entry point for the Black-Scholes PINN project.
+"""
+
+
+def main():
+    """
+    Run the Black-Scholes PINN experiment.
+    """
+
+    print("Black-Scholes PINN experiment")
+
+
+if __name__ == "__main__":
+    main()
